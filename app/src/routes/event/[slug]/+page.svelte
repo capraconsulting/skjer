4722a74090029b560de6eee8a9cbf7ed108e1cbe
Sofie Aasheim <sofieaaf@gmail.com>
2024-05-10 09:17:27 +0200

    convert layout and event page to tw

--- app/src/routes/event/[slug]/+page.svelte
+++ app/src/routes/event/[slug]/+page.svelte
@@ -11,143 +11,36 @@
 	$: ({ data: event } = $q);
 </script>
 
-<section class="event">
+<section class="w-full mt-2 mb-8 mx-0">
 	{#if event.mainImage}
 		<img
-			class="event__cover"
+			class="w-full h-[380px] object-cover"
 			src={urlFor(event.mainImage).url()}
 			alt="Cover image for {event.title}"
 		/>
-	{:else}
-		<div class="event__cover--none" />
 	{/if}
-	<div class="event__container">
-		<h1 class="event__title">{event.title}</h1>
+	<div class="pr-4 pl-3 py-0">
+		<h1 class="text-6xl font-extrabold leading-loose">{event.title}</h1>
 		{#if event.summary}
-			<p class="event__excerpt">{event.summary}</p>
+			<p class="text-2xl">{event.summary}</p>
 		{/if}
-		<p class="event__date">
-			{formatDate(event.start)}
-			{formatTime(event.start)}
-		</p>
-		<div class="event__place">
-			<span class="material-icons">location_on</span>
-			<p class="event__place--text"> {event.place}</p>
+		<div class="my-6 font-bold">
+			<div class="flex">
+				<span class="material-icons mr-2">schedule</span>
+				<p>
+					{formatDate(event.start)}
+					{formatTime(event.start)}
+				</p>
+			</div>
+			<div class="flex">
+				<span class="material-icons mr-2">location_on</span>
+				<p> {event.place}</p>
+			</div>
 		</div>
 		{#if event.body}
-			<div class="event__content">
+			<div class="mt-8 flex flex-col gap-4">
 				<PortableText components={{}} value={event.body} />
 			</div>
 		{/if}
 	</div>
 </section>
-
-<style>
-	.event {
-		width: 100%;
-		margin: var(--space-1) 0 var(--space-4);
-	}
-
-	.event .event__cover,
-	.event .event__cover--none {
-		width: 100%;
-		height: 200px;
-		-o-object-fit: cover;
-		object-fit: cover;
-	}
-
-	.event .event__cover--none {
-		background: var(--black);
-	}
-
-	.event .event__container {
-		padding: 0 var(--space-3);
-	}
-
-	.event .event__content {
-		font-family: var(--font-family-serif);
-		font-weight: 400;
-		font-size: var(--font-size-4);
-		line-height: var(--line-height-5);
-		letter-spacing: -0.02em;
-		margin-top: var(--space-6);
-	}
-
-	.event .event__content blockquote {
-		border-left: 5px solid var(--black);
-		padding-left: var(--space-3);
-		margin-left: var(--space-4);
-	}
-
-	.event .event__content a {
-		color: var(--blue-600);
-		text-decoration: none;
-	}
-
-	.event .event__title {
-		font-family: var(--font-family-sans);
-		font-size: var(--font-size-7);
-		line-height: var(--line-height-6);
-		margin: var(--space-4) 0;
-		font-weight: 800;
-	}
-
-	.event .event__excerpt {
-		font-family: var(--font-family-serif);
-		font-size: var(--font-size-5);
-		line-height: var(--line-height-4);
-		margin-top: 0;
-		font-weight: 400;
-	}
-
-	.event .event__date {
-		font-family: var(--font-family-sans);
-		font-weight: 600;
-		font-family: var(--font-family-sans);
-		font-size: var(--font-size-1);
-		line-height: var(--line-height-1);
-		margin-top: var(--space-4);
-	}
-
-	.event .event__place {
-		display: flex;
-		align-items: center;
-	}
-	.event .event__place--text {
-		font-weight: 400;
-		font-family: var(--font-family-sans);
-		margin-left: 3px;
-	}
-
-	@media (min-width: 800px) {
-		.event .event__cover,
-		.event .event__cover--none {
-			width: 750px;
-			height: 380px;
-		}
-
-		.event .event__title {
-			font-size: var(--font-size-10);
-			line-height: var(--line-height-10);
-			margin: var(--space-6) 0 0;
-			letter-spacing: -0.025em;
-		}
-
-		.event .event__excerpt {
-			font-size: var(--font-size-5);
-			line-height: var(--line-height-5);
-			margin-top: var(--space-3);
-			margin-bottom: var(--space-3);
-		}
-
-		.event .event__date {
-			font-size: var(--font-size-3);
-			line-height: var(--line-height-2);
-			margin-top: var(--space-0);
-		}
-
-		.event .event__content {
-			margin-top: var(--space-7);
-		}
-	}
-</style>
