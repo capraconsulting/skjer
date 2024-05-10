17c11bac68d3c5e23aaf38440ace480d4c88f5b4
Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>
2024-05-10 13:23:05 +0200

    feat: add event participation and update local language

---
+++ studio/api/fetchEventParticipant.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SANITY_STUDIO_SUPABASE_URL!,
+  process.env.SANITY_STUDIO_SUPABASE_KEY!
+);
+
+export async function fetchEventParticipant({ documentId }: { documentId: string }) {
+  try {
+    const { data, error } = await supabase
+      .from("event_participant")
+      .select()
+      .eq("document_id", documentId);
+
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
