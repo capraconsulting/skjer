2d7c8d52bdd71a8d028a67f2c351d871dec8f175
Sofie Aasheim <sofieaaf@gmail.com>
2024-05-10 14:31:57 +0200

    add supabase

--- app/src/components/RegistrationForm.svelte
+++ app/src/components/RegistrationForm.svelte
@@ -10,13 +10,18 @@
 
 
     $: result = {
-		name, email
+		name, email, telephone, firm
 	};
+
+	function subitForm() {
+		console.log(result);
+	};
+
 </script>
 
 <h2 class="text-2xl font-bold pb-4">Meld deg på</h2>
 
-<form class="flex flex-col gap-6" method="POST">
+<form class="flex flex-col gap-6">
     <div class="flex flex-col gap-2">
         <label for="name">Navn</label>
         <input class="border rounded w-full-py-2 px-3 text-gray-700 h-8" type="text" name="name" id="name" bind:value={name} />
@@ -37,5 +42,11 @@
         <input class="border rounded w-full-py-2 px-3 text-gray-700 h-8" type="firm" name="firm" id="firm" bind:value={firm} />
     </div>
     
-    <button class="w-52 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 rounded" type="submit">Send inn</button>
+    <button class="w-52 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 rounded" type="submit" on:click={subitForm}>Send inn</button>
 </form>
+
+<div class="py-8">
+    <pre>
+        {JSON.stringify(result, null, 2)}
+    </pre>
+</div>
