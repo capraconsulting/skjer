c69a99594f14dbbc4bbd26c525278607ac0971ff
Anton Lilleby <antonlilleby@PC04929.localdomain>
2024-05-14 10:13:05 +0200

    chore(tool): vision as dev dependency and format studio code

--- studio/database.types.ts
+++ studio/database.types.ts
@@ -1,114 +1,108 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
 
 export type Database = {
   public: {
     Tables: {
       event_allergies: {
         Row: {
-          allergy: string | null
-          allergy_id: number
-          document_id: string | null
-        }
+          allergy: string | null;
+          allergy_id: number;
+          document_id: string | null;
+        };
         Insert: {
-          allergy?: string | null
-          allergy_id?: number
-          document_id?: string | null
-        }
+          allergy?: string | null;
+          allergy_id?: number;
+          document_id?: string | null;
+        };
         Update: {
-          allergy?: string | null
-          allergy_id?: number
-          document_id?: string | null
-        }
-        Relationships: []
-      }
+          allergy?: string | null;
+          allergy_id?: number;
+          document_id?: string | null;
+        };
+        Relationships: [];
+      };
       event_participant: {
         Row: {
-          accepted_terms: boolean | null
-          attending: boolean | null
-          created_at: string | null
-          document_id: string | null
-          email: string | null
-          firm: string | null
-          full_name: string | null
-          participant_id: number
-          telephone: string | null
-        }
+          accepted_terms: boolean | null;
+          attending: boolean | null;
+          created_at: string | null;
+          document_id: string | null;
+          email: string | null;
+          firm: string | null;
+          full_name: string | null;
+          participant_id: number;
+          telephone: string | null;
+        };
         Insert: {
-          accepted_terms?: boolean | null
-          attending?: boolean | null
-          created_at?: string | null
-          document_id?: string | null
-          email?: string | null
-          firm?: string | null
-          full_name?: string | null
-          participant_id?: number
-          telephone?: string | null
-        }
+          accepted_terms?: boolean | null;
+          attending?: boolean | null;
+          created_at?: string | null;
+          document_id?: string | null;
+          email?: string | null;
+          firm?: string | null;
+          full_name?: string | null;
+          participant_id?: number;
+          telephone?: string | null;
+        };
         Update: {
-          accepted_terms?: boolean | null
-          attending?: boolean | null
-          created_at?: string | null
-          document_id?: string | null
-          email?: string | null
-          firm?: string | null
-          full_name?: string | null
-          participant_id?: number
-          telephone?: string | null
-        }
-        Relationships: []
-      }
+          accepted_terms?: boolean | null;
+          attending?: boolean | null;
+          created_at?: string | null;
+          document_id?: string | null;
+          email?: string | null;
+          firm?: string | null;
+          full_name?: string | null;
+          participant_id?: number;
+          telephone?: string | null;
+        };
+        Relationships: [];
+      };
       participant_options: {
         Row: {
-          option_id: number
-          option_name: string | null
-          option_value: boolean | null
-          participant_id: number | null
-        }
+          option_id: number;
+          option_name: string | null;
+          option_value: boolean | null;
+          participant_id: number | null;
+        };
         Insert: {
-          option_id?: number
-          option_name?: string | null
-          option_value?: boolean | null
-          participant_id?: number | null
-        }
+          option_id?: number;
+          option_name?: string | null;
+          option_value?: boolean | null;
+          participant_id?: number | null;
+        };
         Update: {
-          option_id?: number
-          option_name?: string | null
-          option_value?: boolean | null
-          participant_id?: number | null
-        }
+          option_id?: number;
+          option_name?: string | null;
+          option_value?: boolean | null;
+          participant_id?: number | null;
+        };
         Relationships: [
           {
-            foreignKeyName: "participant_options_participant_id_fkey"
-            columns: ["participant_id"]
-            isOneToOne: false
-            referencedRelation: "event_participant"
-            referencedColumns: ["participant_id"]
+            foreignKeyName: "participant_options_participant_id_fkey";
+            columns: ["participant_id"];
+            isOneToOne: false;
+            referencedRelation: "event_participant";
+            referencedColumns: ["participant_id"];
           },
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -121,66 +115,58 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    ? (PublicSchema["Tables"] & PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
-  PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
-    | { schema: keyof Database },
+  PublicTableNameOrOptions extends keyof PublicSchema["Tables"] | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
-  PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
-    | { schema: keyof Database },
+  PublicEnumNameOrOptions extends keyof PublicSchema["Enums"] | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
     : never = never,
@@ -188,4 +174,4 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    : never;
