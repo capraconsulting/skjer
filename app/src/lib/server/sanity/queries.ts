3d2f78fea20115c36a72b4bc7cd53f424a5f8495
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-03 13:39:10 +0200

    add missing function

--- app/src/lib/server/sanity/queries.ts
+++ app/src/lib/server/sanity/queries.ts
@@ -1,2 +1,11 @@
+import { serverClientWithoutStega } from "$lib/server/sanity/client";
+import { eventQuery } from "$lib/sanity/queries";
+import type { Event } from "$models/sanity.types";
+
+export const getEventContent = async ({ id }: { id: string }) => {
+  const result = await serverClientWithoutStega.fetch<Event>(eventQuery, { id });
+  return result;
+};
+
 export const futureEventsQuery = `*[_type == "event" && start > now() && (!defined($category) || category match $category)] | order(start desc)`;
 export const pastEventsQuery = `*[_type == "event" && start <= now()] | order(start desc)`;
