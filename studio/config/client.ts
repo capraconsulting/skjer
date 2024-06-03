1f51339396098cdaafc386440de852d7d7c1bb1e
Anton Lilleby <antonlilleby@PC04929.localdomain>
2024-06-03 13:10:52 +0200

    feat: add slack webhook

---
+++ studio/config/client.ts
@@ -0,0 +1,15 @@
+import imageUrlBuilder from "@sanity/image-url";
+import { createClient } from "@sanity/client";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+export const client = createClient({
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+  dataset: process.env.SANITY_STUDIO_DATASET,
+  useCdn: true,
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
