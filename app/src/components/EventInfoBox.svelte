3bd9258be58f049cd15e90c1f5e9c3e252b81aa1
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-04 14:53:17 +0200

    replace remaining material icons

--- app/src/components/EventInfoBox.svelte
+++ app/src/components/EventInfoBox.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
   import { formatDate, formatTime } from "$lib/utils";
   import type { Event } from "$models/sanity.model";
+  import { CalendarBlank, Clock, MapPin } from "phosphor-svelte";
 
   export let event: Event;
 </script>
 
 <div
-  class="flex h-full w-full flex-col gap-1 rounded-xl bg-zinc-100 p-3 text-sm font-light sm:p-5 sm:text-base"
+  class="flex h-full w-full flex-col gap-1 rounded-xl bg-zinc-100 p-3 text-sm font-light sm:p-6 sm:text-base"
 >
   <div class="flex items-center">
-    <span class="material-symbols-outlined mr-2 text-base">calendar_today</span>
+    <CalendarBlank class="mr-2" />
     <span>{event.category}</span>
   </div>
 
   <div class="flex items-center">
-    <span class="material-symbols-outlined mr-2 text-base">calendar_today</span>
+    <CalendarBlank class="mr-2" />
     <span>{formatDate(event.start)}</span>
   </div>
 
   <div class="flex items-center">
-    <span class="material-symbols-outlined mr-2 text-base">schedule</span>
+    <Clock class="mr-2" />
     <span>{formatTime(event.start)} - {formatTime(event.end)}</span>
   </div>
 
   <div class="flex items-center">
-    <span class="material-symbols-outlined mr-2 text-base">location_on</span>
+    <MapPin class="mr-2" />
     <span>{event.place}</span>
   </div>
 </div>
