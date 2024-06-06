e0db3f7c69fb544468def7018d4d0d8278823b85
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-06 14:12:02 +0200

    adjust settings

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -8,14 +8,9 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:5173/",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
