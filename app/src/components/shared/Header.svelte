aac1a72c9a7bf313253f1de9b28e6e932772566c
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-07 15:55:38 +0200

    comment out safari check

--- app/src/components/shared/Header.svelte
+++ app/src/components/shared/Header.svelte
@@ -11,7 +11,7 @@
   $: isRoot = $page.url.pathname === "/";
 
   // Check for Safari and iOS devices due to performance issues with large WebP images.
-  $: isSafariOrIOS = isSafari() || isIOS();
+  /*   $: isSafariOrIOS = isSafari() || isIOS();
 
   function isSafari() {
     const userAgent = navigator.userAgent;
@@ -22,7 +22,7 @@
   function isIOS() {
     const userAgent = navigator.userAgent;
     return /iPad|iPhone/.test(userAgent);
-  }
+  } */
 </script>
 
 <header class="flex h-[100px] w-full items-center justify-between px-4 pt-2 lg:px-20">
@@ -30,12 +30,12 @@
     <img
       class="block h-12 select-none sm:h-14 dark:hidden"
       alt="Animert Capra, Fryde og Liflig-logo"
-      src={isSafariOrIOS ? logoSm : logo}
+      src={logo}
     />
     <img
       class="hidden h-12 select-none sm:h-14 dark:block"
       alt="Animert Capra, Fryde og Liflig-logo"
-      src={isSafariOrIOS ? logoSmDark : logoDark}
+      src={logoDark}
     />
   </a>
   <div class="flex flex-row items-center justify-end gap-2">
