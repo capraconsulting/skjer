0ea1f5b3f22303d3c63b43c63c71821510ea93b0
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-10 10:13:40 +0200

    add personverneklæring

--- app/src/components/shared/Footer.svelte
+++ app/src/components/shared/Footer.svelte
@@ -2,30 +2,40 @@
   import capraLogoWhite from "$lib/assets/capra-white-vertical.webp";
   import frydeLogoWhite from "$lib/assets/fryde-white-vertical.webp";
   import lifligLogoWhite from "$lib/assets/liflig-white-vertical.webp";
-  import capraLogoBlack from "$lib/assets/capra-black-vertical.webp";
-  import frydeLogoBlack from "$lib/assets/fryde-black-vertical.webp";
-  import lifligLogoBlack from "$lib/assets/liflig-black-vertical.webp";
+  import miljo from "$lib/assets/miljofyrtarn.png";
+  import dnv from "$lib/assets/dnv.png";
 </script>
 
 <footer
-  class="gap-8py-16 flex h-[470px] w-full flex-col items-center justify-center text-lg font-light sm:gap-12"
+  class="gap-8py-16 flex h-[470px] w-full flex-col items-center justify-center bg-black text-lg font-light text-white sm:gap-12"
 >
   <p class="pb-2 pt-6">Vi er en del av Capra-gruppen</p>
-  <div class="flex gap-11">
-    <a href="https://www.capraconsulting.no/">
-      <img class="block h-20 sm:h-28 dark:hidden" alt="Capra-logo" src={capraLogoBlack} />
-      <img class="hidden h-20 sm:h-28 dark:block" alt="Capra-logo" src={capraLogoWhite} />
-    </a>
-    <a href="https://www.liflig.no/">
-      <img class="block h-20 sm:h-28 dark:hidden" alt="Liflig-logo" src={lifligLogoBlack} />
-      <img class="hidden h-20 sm:h-28 dark:block" alt="Liflig-logo" src={lifligLogoWhite} />
-    </a>
-    <a href="https://www.fryde.no/">
-      <img class="block h-20 sm:h-28 dark:hidden" alt="Fryde-logo" src={frydeLogoBlack} />
-      <img class="hidden h-20 sm:h-28 dark:block" alt="Fryde-logo" src={frydeLogoWhite} />
-    </a>
+  <div class="flex w-full flex-col items-center justify-evenly sm:flex-row">
+    <div class="flex flex-col items-center justify-center gap-4 font-normal">
+      <div>Sertifiseringer</div>
+      <div class="flex gap-2">
+        <img class="h-20" alt="Miljøfyrtårn-sertifisering" src={miljo} />
+        <img class="h-20" alt="DNV-sertifisering" src={dnv} />
+      </div>
+    </div>
+    <div class="flex gap-11">
+      <a href="https://www.capraconsulting.no/">
+        <img class="h-20 sm:h-28" alt="Capra-logo" src={capraLogoWhite} />
+      </a>
+      <a href="https://www.liflig.no/">
+        <img class="h-20 sm:h-28" alt="Liflig-logo" src={lifligLogoWhite} />
+      </a>
+      <a href="https://www.fryde.no/">
+        <img class="h-20 sm:h-28" alt="Fryde-logo" src={frydeLogoWhite} />
+      </a>
+    </div>
+    <div>
+      <a href="/personvern" class="font-normal underline">Personvernerklæring</a>
+    </div>
   </div>
   <p class="pt-10 text-center text-sm sm:text-base">
-    2024 © Capra | Stenersgata 2 | N-0168 Oslo | post@capraconsulting.no
+    2024 © Capra | Stenersgata 2 | N-0168 Oslo | <a href="mailto:post@capraconsulting.no"
+      >post@capraconsulting.no</a
+    >
   </p>
 </footer>
---
+++ app/src/routes/personvern/+page.svelte
@@ -0,0 +1,182 @@
+<svelte:head>
+  <title>Personvernerklæring | Capra Liflig Fryde</title>
+</svelte:head>
+
+<section class="text-lg font-light sm:w-[80%]">
+  <h1 class="pb-4 pt-8 text-5xl font-light">Personvernerklæring</h1>
+
+  <p class="py-4">
+    Denne personvernerklæringen forteller hvordan Capra AS, Liflig AS og Fryde AS samler inn og
+    bruker personopplysninger.
+  </p>
+
+  <p class="py-4">
+    Capra AS, Liflig AS og Fryde AS, ved daglige ledere, er behandlingsansvarlig for virksomhetens
+    behandling av personopplysninger. Der det daglige ansvaret er delegert, kommer det fram under
+    hvert enkelt punkt. Delegeringen omfatter kun oppgavene og ikke ansvaret. Erklæringen inneholder
+    opplysninger du har krav på når det samles inn opplysninger fra nettstedet vårt,
+    rekrutteringssystemet, salgssystemet (personopplysningsloven § 19) og generell informasjon om
+    hvordan vi behandler personopplysninger (personopplysningsloven § 18 1. ledd).
+  </p>
+
+  <h2 class="pt-6 text-3xl">Behandling av personopplysninger for ikke-ansatte</h2>
+  <p class="py-4">
+    Det er 3 tilfeller hvor Capra, Liflig og Fryde innhenter persondata om eksterne/ikke-ansatte.
+    Det ene tilfellet er rekruttering, hvor kandidater legges inn i rekrutteringssystemet. Det andre
+    og tredje tilfellet er i salgssammenheng, hvor henholdsvis underkonsulenter og kunder legges inn
+    i salgssystemet. I de to første tilfellene kan en kandidat/kunde/underkonsulent havne i
+    systemene våre på to måter: a) Ved å registrere seg selv gjennom en utlyst søknad, eller b)
+    Vedkommende blir lagt inn som følge av tips fra andre eller sourcing (LinkedIn,
+    rekrutteringsbyrå, etc.). For kunder legges kontaktinfo inn så snart vi er i forhandlingsprosess
+    med dem.
+  </p>
+
+  <h2 class="pt-6 text-3xl">Rekruttering</h2>
+  <p class="py-4">
+    Avhengig av hvilke opplysninger kandidaten gir oss eller vi finner om kandidaten, samler vi
+    blant annet inn følgende opplysninger: Navn, e-postadresse, telefonnummer, utdannelse,
+    arbeidserfaring, URLer til relevante sosiale medium (som LinkedIn, GitHub, mv.),
+    referansepersoner og opplastede dokumenter (som CV, søknadsbrev, karakterer, mv.).
+  </p>
+
+  <h3 class="pt-4 text-2xl">Kandidat registrerer seg selv</h3>
+  <ul class="list-inside list-disc space-y-2 py-4 ps-5">
+    <li>Kandidaten må godta samtykke for å kunne sende inn søknad via søknadssidene våre.</li>
+    <li>
+      Opplysninger oppbevares i systemene så lenge de er oppdaterte og det er relevant å beholde
+      dem, men maksimalt 2 år såfremt samtykke er innhentet.
+    </li>
+  </ul>
+
+  <h3 class="pt-4 text-2xl">
+    Kandidat blir tipset om til Capra, Liflig eller Fryde uten sin viten
+  </h3>
+  <ul class="list-inside list-disc space-y-2 py-4 ps-5">
+    <li>
+      Vi tar kontakt med kandidater gjennom tips fra egne søk, nettverkstreff, anbefaling fra
+      ansatte eller videresendte fra rekrutteringsbyrå.
+    </li>
+    <li>
+      Vi innhenter samtykke senest 30 dager fra kandidatenes data blir behandlet av rekruttering.
+      Opplysninger oppbevares i systemene så lenge de er oppdaterte og relevante å beholdemen
+      maksimalt 2 år såfremt samtykke er innhentet.
+    </li>
+    <li>Uten innhentet samtykke blir kandidaten slettet etter 30 dager.</li>
+    <li>
+      Kandidater som er blitt tipset av ansatte før innføringen av den nye personvernlovgivningen
+      har mottatt en informasjonsmail og vil bli slettet etter ønske fra kandidaten eller automatisk
+      etter 2 år fra den nye personvernlovgivningen trådte i kraft.
+    </li>
+  </ul>
+
+  <h2 class="pt-6 text-3xl">Salg</h2>
+  <p class="py-4">
+    Avhengig av hvilke opplysninger kunde eller underkonsulent gir oss eller vi finner om de, samler
+    vi blant annet inn følgende opplysninger: Navn, e-postadresse, telefonnummer, utdannelse,
+    arbeidserfaring.
+  </p>
+
+  <h3 class="pt-4 text-2xl">Kundeinformasjon</h3>
+  <ul class="list-inside list-disc space-y-2 py-4 ps-5">
+    <li>
+      I Capra, Liflig og Fryde lagrer vi personopplysninger om KAM og andre relevante
+      kontaktpersoner hos kunde. Disse lagres så lenge kundeforholdet vedvarer.
+    </li>
+  </ul>
+
+  <h3 class="pt-4 text-2xl">Underkonsulent registrerer seg selv</h3>
+  <ul class="list-inside list-disc space-y-2 py-4 ps-5">
+    <li>
+      Underkonsulenten får en automatisk generert beskjed om å samtykke til oppbevaring av
+      persondata.
+    </li>
+    <li>
+      Etter 10 år vil underkonsulenten få et varsel om man fremdeles ønsker å være registrert i vår
+      database. Om de ikke ønsker dette, vil de bli slettet.
+    </li>
+  </ul>
+
+  <h3 class="pt-4 text-2xl">
+    Underkonsulent blir tipset om til Capra, Liflig eller Fryde uten sin viten.
+  </h3>
+  <ul class="list-inside list-disc space-y-2 py-4 ps-5">
+    <li>Salgsansvarlig har da ansvar for ukentlig å gå over underkonsulenter som er tipset om.</li>
+    <li>
+      Det skal da, senest 7 dager etter tips er registrert sendes ut en mail med info om
+      personopplysningene vi har, med en forespørsel om å beholde disse
+    </li>
+    <li>
+      Om det ikke mottas samtykke innen 14 dager, vil underkonsulent bli slettet fra databasen.
+    </li>
+  </ul>
+
+  <h2 class="pt-6 text-3xl">Behandling av personopplysninger for ansatte</h2>
+  <p class="py-4">
+    Capra AS, Liflig AS og Fryde AS behandler personopplysninger om sine ansatte for å administrere
+    lønn og personalansvar. Rettslig grunnlag følger av personopplysningsloven § 8, første ledd og §
+    8 a), b) eller f) samt § 9 a), b) og f). Det er Administrasjonen v/økonomiansvarlig som har det
+    daglige ansvaret for dette. Det registreres nødvendige opplysninger for utbetaling av lønn, for
+    eksempel grunndata, lønnsnivå, tidsregistrering, skatteprosent, skattekommune og
+    fagforeningstilhørighet. Andre opplysninger om ansatte er knyttet til vedkommendes
+    arbeidsinstruks og tilrettelegging av vedkommendes arbeid.
+  </p>
+  <p class="py-4">
+    Dessuten registreres det opplysninger i tilknytning til nøkkelkortadministrasjon av inn- og
+    utpasseringer og opplysninger om tilgangsstyring i IT-systemene våre. Opplysningene hentes fra
+    de ansatte selv. Opplysningene utleveres bare i forbindelse med lønnsutbetalinger og andre
+    lovpliktige utleveringer. Sletterutiner for personalopplysninger følger regnskapsloven og
+    arkivloven, all persondata vi ikke kan lagre under disse lovene fjernes etter siste arbeidsdag.
+    Opplysninger om navn, stilling og arbeidsområde regnes å være offentlige opplysninger og kan
+    publiseres på Capra AS, Liflig AS eller Fryde AS sine nettsider. Informasjon om den ansatte, som
+    ikke kreves arkivert av regnskapsloven og arkivloven, vil slettes så snart ansatte har
+    gjennomført siste arbeidsdag.
+  </p>
+  <p class="py-4">
+    I Capra, Liflig og Fryde innhenter vi følgende personopplysninger fra våre ansatte til bruk i
+    våre interne IT-systemer:
+  </p>
+  <ul class="list-inside list-disc space-y-2 py-4 ps-5">
+    <li>Fornavn</li>
+    <li>Etternavn</li>
+    <li>Personnummer</li>
+    <li>Adresse</li>
+    <li>E-post</li>
+    <li>Telefonnr</li>
+    <li>Kontonummer</li>
+    <li>Tidligere arbeidsgiver</li>
+    <li>Nasjonalitet</li>
+  </ul>
+
+  <h2 class="pt-6 text-3xl">Sikkerhet</h2>
+  <p class="py-4">
+    Hver enkelt tredjepartssystem ivaretar sikkerheten for den dataen de behandler. Dette skal være
+    dokumentert i henhold til hver enkelt leverandør av tredjepartssystemer sin databehandleravtale
+    med Capra AS, Liflig AS og Fryde AS.
+  </p>
+
+  <h2 class="pt-6 text-3xl">Rettigheter</h2>
+  <p class="py-4">
+    Alle som spør har rett på grunnleggende informasjon om behandlinger av personopplysninger i en
+    virksomhet etter personopplysningslovens § 18, 1. ledd. Capra AS, Liflig AS og Fryde AS har gitt
+    denne informasjonen i denne erklæringen, og vil henvise til den ved eventuelle forespørsler. De
+    som er registrert i en av Capra AS, Liflig AS og Fryde AS sine systemer har rett på innsyn i
+    egne opplysninger. Vedkommende har også rett til å be om at uriktige, ufullstendige eller
+    opplysninger Capra AS, Liflig AS og Fryde AS ikke har adgang til å behandle blir rettet, slettet
+    eller supplert. Krav fra den registrerte skal besvares kostnadsfritt og senest innen 30 dager.
+  </p>
+
+  <h2 class="pt-6 text-3xl">Kontakt oss</h2>
+  <p class="py-4">
+    For henvendelser knyttet til rettighetene til den registrerte, kan Capra AS, Liflig AS og Fryde
+    AS kontaktes på: <br /><br />
+
+    Capra AS <br />
+    v/personvernansvarlig <br />
+    Stenersgata 2 N-0184 <br />
+    Oslo <br /> <br />
+
+    <a href="mailto:gdpr@capraconsulting.no" class="font-normal underline"
+      >gdpr@capraconsulting.no</a
+    >
+  </p>
+</section>
