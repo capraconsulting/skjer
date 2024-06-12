17f1b6cb223a14f14bc8e4174321df730041ab14
Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>
2024-06-12 10:35:31 +0200

    fix: disable button when trying to sign in or out

--- app/src/components/shared/SignInOrOut.svelte
+++ app/src/components/shared/SignInOrOut.svelte
@@ -3,16 +3,34 @@
   import { Button } from "flowbite-svelte";
 
   export let auth;
+  let isSigningOut = false; // Reactive variable for the sign-out button
+  let isSigningIn = false; // Reactive variable for the sign-in button
 </script>
 
 {#if auth}
   <div class="flex flex-row items-center gap-2">
     <img class="h-7 rounded-2xl" alt="Profilbilde" src={auth.user.image} />
     <span class="mr-6 hidden text-sm font-normal sm:block">{auth.user.name}</span>
-    <Button color="dark" class="h-7" pill on:click|once={() => signOut({ callbackUrl: "/" })}
-      >Logg ut</Button
+    <Button
+      color="dark"
+      class="h-7"
+      pill
+      on:click={() => {
+        isSigningOut = true;
+        signOut({ callbackUrl: "/" });
+      }}
+      disabled={isSigningOut}>Logg ut</Button
     >
   </div>
 {:else}
-  <Button color="dark" class="h-7" pill on:click|once={() => signIn("google")}>Logg inn</Button>
+  <Button
+    color="dark"
+    class="h-7"
+    pill
+    on:click={() => {
+      isSigningIn = true;
+      signIn("google");
+    }}
+    disabled={isSigningIn}>Logg inn</Button
+  >
 {/if}
