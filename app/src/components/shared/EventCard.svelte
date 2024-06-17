6c7951570459dd8e9abc35016f25653cd8ddf339
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-17 10:25:34 +0200

    fix alignment event cards

--- app/src/components/shared/EventCard.svelte
+++ app/src/components/shared/EventCard.svelte
@@ -20,15 +20,17 @@
       />
     {/if}
 
-    <div class="mx-4 my-4 flex flex-col gap-2">
-      <h2 class="text-2xl font-semibold">
-        {event.title}
-      </h2>
-      {#if event.summary}
-        <p class="text-base font-light sm:text-lg">{event.summary}</p>
-      {/if}
+    <div class="mx-4 my-4 grid h-full grid-cols-1 content-between gap-2">
+      <div class="flex flex-col gap-2">
+        <h2 class="text-2xl font-semibold">
+          {event.title}
+        </h2>
+        {#if event.summary}
+          <p class="text-base font-light sm:text-lg">{event.summary}</p>
+        {/if}
+      </div>
 
-      <div class="mt-5 flex items-center justify-between gap-6">
+      <div class="self mt-5 flex justify-between gap-6">
         <EventLogos {event} />
         <div class="flex flex-row items-center gap-2">
           <p>Se mer</p>
