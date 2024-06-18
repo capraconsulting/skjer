355cddaf13fd30f4ebfba090021428a51287d082
an2n <an2n@users.noreply.github.com>
2024-06-18 16:29:12 +0200

    fix: attempt fixing cors in hook (#61)
    Co-authored-by: Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>

--- app/src/lib/auth/api.ts
+++ app/src/lib/auth/api.ts
@@ -1,22 +1,25 @@
 import { PUBLIC_SANITY_STUDIO_URL } from "$env/static/public";
 import type { Handle } from "@sveltejs/kit";
 
-export const createAPISecurityPolicy: Handle = async ({ resolve, event }) => {
-  if (event.url.pathname.startsWith("/api")) {
-    if (event.request.method === "OPTIONS") {
-      return new Response(null, {
-        headers: {
-          "Access-Control-Allow-Methods": "POST",
-          "Access-Control-Allow-Origin": PUBLIC_SANITY_STUDIO_URL,
-          "Access-Control-Allow-Headers": "*",
-        },
-      });
+const corsHeaders = {
+  "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Allow-Origin": PUBLIC_SANITY_STUDIO_URL,
+  "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+  "Access-Control-Allow-Headers":
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+};
+
+export const createCorsHandler: Handle = async ({ event, resolve }) => {
+  if (event.request.method !== "OPTIONS") {
+    const response = await resolve(event);
+
+    const headers = new Headers(response.headers);
+
+    for (const [key, value] of Object.entries(corsHeaders)) {
+      headers.set(key, value);
     }
+    return response;
   }
 
-  const response = await resolve(event);
-  if (event.url.pathname.startsWith("/api")) {
-    response.headers.append("Access-Control-Allow-Origin", PUBLIC_SANITY_STUDIO_URL);
-  }
-  return response;
+  return new Response("OK", { headers: corsHeaders });
 };
