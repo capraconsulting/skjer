6f3002ad6992081dd050a00384ed0d840e40e19c
Anton Lilleby <antonlilleby@PC04929.localdomain>
2024-06-21 15:19:54 +0200

    fix: logos

--- app/src/components/shared/Footer.svelte
+++ app/src/components/shared/Footer.svelte
@@ -1,32 +1,50 @@
 <script>
   import capraLogoWhite from "$lib/assets/capra-white-vertical.webp";
+  import capraLogoBlack from "$lib/assets/capra-black-vertical.webp";
   import frydeLogoWhite from "$lib/assets/fryde-white-vertical.webp";
+  import frydeLogoBlack from "$lib/assets/fryde-black-vertical.webp";
   import lifligLogoWhite from "$lib/assets/liflig-white-vertical.webp";
-  import miljo from "$lib/assets/miljofyrtarn.webp";
-  import dnv from "$lib/assets/dnv.webp";
+  import lifligLogoBlack from "$lib/assets/liflig-black-vertical.webp";
+  import miljofyrtarnDark from "$lib/assets/miljofyrtarn-dark.webp";
+  import miljofyrtarnLight from "$lib/assets/miljofyrtarn-light.webp";
+  import dnvDark from "$lib/assets/dnv-dark.webp";
+  import dnvLight from "$lib/assets/dnv-light.webp";
 </script>
 
 <footer
-  class="gap-8py-16 flex h-[470px] w-full flex-col items-center justify-center bg-black text-lg font-light text-white sm:gap-12"
+  class="gap-8py-16 flex h-[470px] w-full flex-col items-center justify-center text-lg font-light sm:gap-12"
 >
   <p class="pb-2 pt-6">Vi er en del av Capra-gruppen</p>
   <div class="flex w-full flex-col items-center justify-evenly gap-6 sm:flex-row sm:gap-0">
     <div class="flex flex-col items-center justify-center gap-4 font-normal">
       <div class="pt-6 text-base sm:p-0 sm:text-lg">Sertifiseringer</div>
       <div class="flex gap-2">
-        <img class="h-16 sm:h-20" alt="Miljøfyrtårn-sertifisering" src={miljo} />
-        <img class="h-16 sm:h-20" alt="DNV-sertifisering" src={dnv} />
+        <img
+          class="block h-16 sm:h-20 dark:hidden"
+          alt="Miljøfyrtårn-sertifisering"
+          src={miljofyrtarnDark}
+        />
+        <img
+          class="hidden h-16 sm:h-20 dark:block"
+          alt="Miljøfyrtårn-sertifisering"
+          src={miljofyrtarnLight}
+        />
+        <img class="block h-16 sm:h-20 dark:hidden" alt="DNV-sertifisering" src={dnvDark} />
+        <img class="hidden h-16 sm:h-20 dark:block" alt="DNV-sertifisering" src={dnvLight} />
       </div>
     </div>
     <div class="flex gap-11">
       <a href="https://www.capraconsulting.no/">
-        <img class="h-20 sm:h-28" alt="Capra-logo" src={capraLogoWhite} />
+        <img class="block h-20 sm:h-28 dark:hidden" alt="Capra-logo" src={capraLogoBlack} />
+        <img class="hidden h-20 sm:h-28 dark:block" alt="Capra-logo" src={capraLogoWhite} />
       </a>
       <a href="https://www.liflig.no/">
-        <img class="h-20 sm:h-28" alt="Liflig-logo" src={lifligLogoWhite} />
+        <img class="block h-20 sm:h-28 dark:hidden" alt="Liflig-logo" src={lifligLogoBlack} />
+        <img class="hidden h-20 sm:h-28 dark:block" alt="Liflig-logo" src={lifligLogoWhite} />
       </a>
       <a href="https://www.fryde.no/">
-        <img class="h-20 sm:h-28" alt="Fryde-logo" src={frydeLogoWhite} />
+        <img class="block h-20 sm:h-28 dark:hidden" alt="Fryde-logo" src={frydeLogoBlack} />
+        <img class="hidden h-20 sm:h-28 dark:block" alt="Fryde-logo" src={frydeLogoWhite} />
       </a>
     </div>
     <div>
