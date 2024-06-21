36d1682292cba7d3fe79f448ca1d0c918150a4f8
Sofie Aasheim <sofieaaf@gmail.com>
2024-06-21 12:30:41 +0200

    fix spacing

--- app/src/components/shared/EventBadges.svelte
+++ app/src/components/shared/EventBadges.svelte
@@ -8,27 +8,23 @@
 
 <div class="flex flex-wrap items-center gap-2">
   {#if event.openForExternals}
-    <Badge
-      rounded
-      class=" h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800"
+    <Badge rounded class="h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800"
       >For alle</Badge
     >
   {:else}
-    <Badge
-      rounded
-      class=" h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800"
+    <Badge rounded class="h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800"
       >Kun interne</Badge
     >
   {/if}
-  <Badge rounded class=" h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800"
+  <Badge rounded class="h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800"
     >{event.category}</Badge
   >
-  <Badge rounded class=" h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800">
+  <Badge rounded class="h-6 whitespace-nowrap border border-black bg-transparent dark:bg-zinc-800">
     {formatDate(event.start)}
   </Badge>
 
   {#if event.attending}
-    <Badge rounded class="bg-yellowSpark  h-6 whitespace-nowrap border-none text-black"
+    <Badge rounded class="bg-yellowSpark h-6 whitespace-nowrap border-none text-black"
       >Du er påmeldt</Badge
     >
   {/if}
