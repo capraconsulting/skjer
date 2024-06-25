103a4c1c36ae9ebd5f78da18e5b0bb91c891e4fb
an2n <an2n@users.noreply.github.com>
2024-06-25 13:05:49 +0200

    Style/logos (#75)
    Co-authored-by: Anton Lilleby <antonlilleby@PC04929.localdomain>
    Co-authored-by: Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>

--- app/src/components/shared/Header.svelte
+++ app/src/components/shared/Header.svelte
@@ -1,8 +1,8 @@
 <script>
-  import logo from "$lib/assets/logo.webp";
+  import logoLight from "$lib/assets/logo-light.webp";
   import logoDark from "$lib/assets/logo-dark.webp";
-  import logoSm from "$lib/assets/logo-sm.webp";
-  import logoSmDark from "$lib/assets/logo-dark-sm.webp";
+  import logoDarkSm from "$lib/assets/logo-dark-sm.webp";
+  import logoLightSm from "$lib/assets/logo-light-sm.webp";
   import SignInOrOut from "$components/shared/SignInOrOut.svelte";
   import { page } from "$app/stores";
   import { DarkMode } from "flowbite-svelte";
@@ -17,22 +17,20 @@
 
 <header class="flex h-[100px] w-full items-center justify-between px-4 pt-2 lg:px-20">
   <a
-    class={isRoot
-      ? "pointer-events-none flex items-center"
-      : "pointer-events-auto flex items-center"}
+    class="{`flex items-center ${isRoot ? 'pointer-events-none' : 'pointer-events-auto'}`}}"
     href="/"
   >
     <img
       fetchpriority="high"
       class="absolute h-12 select-none opacity-100 sm:h-14 dark:opacity-5"
       alt="Animert Capra, Fryde og Liflig-logo"
-      src={hasPerformanceIssue ? logoSm : logo}
+      src={hasPerformanceIssue ? logoDarkSm : logoDark}
     />
     <img
       fetchpriority="high"
       class="absolute h-12 select-none opacity-5 sm:h-14 dark:opacity-100"
       alt="Animert Capra, Fryde og Liflig-logo"
-      src={hasPerformanceIssue ? logoSmDark : logoDark}
+      src={hasPerformanceIssue ? logoLightSm : logoLight}
     />
   </a>
 
