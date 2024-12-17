8d327a0737bc331683b73f65fe4e8f6b5b3d42b9
Fredrik Gram Larsen <fglarse@gmail.com>
2024-12-17 12:53:33 +0100

    feat: update email templates

--- app/src/lib/email/event/canceled.ts
+++ app/src/lib/email/event/canceled.ts
@@ -22,7 +22,8 @@ export const sendCanceled = async (props: EventProps) => {
   const icsFile = createIcsFile(props);
   const mailParams = createMailParams({
     ...props,
-    subject: `Avlyst arrangement: ${props.summary}`,
+    subject: `Viktig beskjed: Arrangementet er dessverre avlyst`,
+    html,
     icsFile,
   });
 
@@ -41,6 +42,24 @@ const createIcsFile = ({
   mailTo,
 }: EventProps) => {
   const url = `${PUBLIC_APP_BASE_URL}/event/${id}`;
+
+  const html = `<span style="font-family: Roboto, sans-serif;
+                             font-style: normal;
+                             font-weight: 400;
+                             font-size: 14px;
+                             line-height: 20px;
+                             letter-spacing: 0.2px;
+                             color: #3c4043;">
+                <p>Ånei! Arrangementet er dessverre avlyst. Vi hadde gledet oss til å ta deg imot og beklager
+                ulempene dette måtte medføre.</p>
+                <p>Vi vet at mange hadde gledet seg til arrangementet, og vi deler deres skuffelse.</p>
+                <p>Hvis du har noen spørsmål eller trenger ytterligere informasjon, er du hjertelig velkommen
+                til å ta kontakt med oss.</p>
+                <p>Vi setter stor pris på din forståelse.</p>
+                <p>Vennlig hilsen,</p>
+                <p>oss i Capra, Liflig og Fryde</p>
+                <p>P.S. Vi lover å gjøre det neste arrangementet verdt ventetiden!</p>
+                </span>`;
 
   const calendar = ical({ name: organiser, method: ICalCalendarMethod.CANCEL });
   calendar.createEvent({
@@ -67,11 +86,12 @@ const createIcsFile = ({
   return Buffer.from(calendar.toString());
 };
 
-const createMailParams = ({ organiser, mailTo, subject, icsFile }: EmailParams) => {
+const createMailParams = ({ organiser, mailTo, subject, html, icsFile }: EmailParams) => {
   return {
     from: `${organiser} <no-reply@capraconsulting.no>`,
     to: mailTo,
     subject,
+    html,
     icalEvent: {
       method: "request",
       content: icsFile,
