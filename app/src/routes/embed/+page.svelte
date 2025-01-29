a32b89e0c43ba8862e3ee9bb5cf4a6fe0ea60431
Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>
2025-01-29 13:29:56 +0100

    fix: update resize package

--- app/src/routes/embed/+page.svelte
+++ app/src/routes/embed/+page.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
-  import "@iframe-resizer/child";
   import EventListItem from "$components/shared/EventListItem.svelte";
 
   export let data;
   let { futureEvents } = data;
 </script>
+
+<svelte:head>
+  <script
+    type="module"
+    src="https://cdn.jsdelivr.net/npm/@open-iframe-resizer/core@latest/dist/index.js"
+  ></script>
+</svelte:head>
 
 {#if futureEvents.length}
   <div class="flex flex-col gap-4">
