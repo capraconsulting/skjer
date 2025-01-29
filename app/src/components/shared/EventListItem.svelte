e15ed42de192e9123c601223f0426388b1f9c6c4
an2n <an2n@users.noreply.github.com>
2025-01-29 11:56:33 +0100

    fix: open embed events in new links (#173)
    Co-authored-by: Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>

--- app/src/components/shared/EventListItem.svelte
+++ app/src/components/shared/EventListItem.svelte
@@ -5,12 +5,14 @@
   import EventBadges from "./EventBadges.svelte";
 
   export let event: EventWithAttending;
+  export let target: string = "_self"; // Default to "_self" if not provided
 </script>
 
 <a
-  class="flex flex-col justify-between rounded-md px-3 py-4 hover:transition-[3s] border border-gray-200 hover:border-gray-800 dark:border-zinc-800
+  class="flex flex-col justify-between rounded-md border border-gray-200 px-3 py-4 hover:border-gray-800 hover:transition-[3s] dark:border-zinc-800
   dark:bg-zinc-800 dark:hover:bg-zinc-700 md:flex-row"
   href={`/event/${event._id}`}
+  {target}
 >
   <div class="flex flex-col gap-3 px-0 font-light md:flex-row lg:gap-2 lg:px-2">
     <h2 class="max-w-md truncate whitespace-pre-wrap pr-3 text-xl">
