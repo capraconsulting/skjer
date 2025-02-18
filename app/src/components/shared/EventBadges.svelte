2a2a19baabcf7d98d4933694d6da05d8d7ce6963
Anton Lilleby <antonlilleby@Anton-sin-MacBook-Pro.local>
2025-02-18 13:06:34 +0100

    feat: enhance display of today date tag

--- app/src/components/shared/EventBadges.svelte
+++ app/src/components/shared/EventBadges.svelte
@@ -4,38 +4,46 @@
   import type { EventWithAttending } from "$models/databaseView.model";
 
   export let event: EventWithAttending;
+  const startDate = formatDate(event.start);
 </script>
 
 <div class="flex flex-wrap items-center gap-2">
   {#if event.openForExternals}
     <Badge
       class="h-6 whitespace-nowrap rounded-lg border border-gray-300 bg-transparent dark:bg-zinc-800"
-      >For alle</Badge
     >
+      For alle
+    </Badge>
   {:else}
     <Badge
       rounded
       class="h-6 whitespace-nowrap rounded-lg border border-gray-300 bg-transparent dark:bg-zinc-800"
-      >Kun interne</Badge
     >
+      Kun interne
+    </Badge>
   {/if}
   {#if event.category}
     <Badge
       rounded
       class="h-6 whitespace-nowrap rounded-lg border border-gray-300 bg-transparent dark:bg-zinc-800"
-      >{event.category}</Badge
     >
+      {event.category}
+    </Badge>
   {/if}
   <Badge
     rounded
-    class="h-6 whitespace-nowrap rounded-lg border border-gray-300 bg-transparent dark:bg-zinc-800"
+    class={`h-6 whitespace-nowrap rounded-lg border ${
+      startDate === "I dag"
+        ? "border-transparent bg-zinc-800 text-white dark:bg-white dark:text-black"
+        : "border-gray-300 bg-transparent dark:bg-zinc-800"
+    }`}
   >
-    {formatDate(event.start)}
+    {startDate}
   </Badge>
 
   {#if event.attending}
-    <Badge rounded class="h-6 whitespace-nowrap rounded-lg border-none bg-yellowSpark text-black"
-      >Du er påmeldt</Badge
-    >
+    <Badge rounded class="bg-yellowSpark h-6 whitespace-nowrap rounded-lg border-none text-black">
+      Du er påmeldt
+    </Badge>
   {/if}
 </div>
