d7ed4f12b45123d3d32fca0e51d02400f9592f28
Anne-Margrethe Sæther <anne.saether@gmail.com>
2025-08-28 16:34:06 +0200

    Ny kommende arrangementer layout med ryddigere grid struktur

---
+++ app/src/components/shared/CompanyLogos.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Event } from "$models/sanity.model";
+  import capraLogo from "$lib/assets/Capra_symbol_RGB.png";
+  import frydeLogo from "$lib/assets/Fryde_symbol_RGB.png";
+  import lifligLogo from "$lib/assets/Liflig_symbol_RGB.png";
+
+  export let event: Event;
+  export let height: number;
+</script>
+
+<div class="flex w-max">
+  {#if event.organisers === "Alle"}
+    <div class="flex gap-2">
+      <img class="h-{height}" alt="Capra logo" src={capraLogo} />
+      <img class="h-{height}" alt="Liflig logo" src={lifligLogo} />
+      <img class="h-{height}" alt="Fryde logo" src={frydeLogo} />
+    </div>
+  {:else if event.organisers === "Capra"}
+    <img class="flex h-{height}" alt="Capra logo" src={capraLogo} />
+  {:else if event.organisers === "Liflig"}
+    <img class="flex h-{height}" alt="Liflig logo" src={lifligLogo} />
+  {:else if event.organisers === "Fryde"}
+    <img class="flex h-{height}" alt="Fryde logo" src={frydeLogo} />
+  {/if}
+</div>
--- app/src/components/shared/EventListItem.svelte
+++ app/src/components/shared/EventListItem.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
-  import { ArrowRight } from "phosphor-svelte";
   import type { EventWithAttending } from "$models/databaseView.model";
-  import EventLogos from "$components/shared/EventLogos.svelte";
   import EventBadges from "./EventBadges.svelte";
-
   export let event: EventWithAttending;
   export let target: string | null = null;
 </script>
 
 <li>
   <a
-    class="flex flex-col justify-between gap-3 rounded-md border border-gray-200 px-3 py-4 hover:border-gray-800 hover:transition-[3s] dark:border-zinc-800
-    dark:bg-zinc-800 dark:hover:bg-zinc-700 md:flex-row"
+    class="flex flex-col justify-between gap-3 rounded-md border border-gray-200 px-3 py-4 hover:border-gray-800 hover:transition-[3s] md:flex-row
+    dark:border-zinc-800 dark:bg-zinc-800 dark:hover:bg-zinc-700"
     href={`/event/${event._id}`}
     {target}
   >
-    <div class="flex flex-col gap-4 font-light lg:flex-row">
-      <h2 class="max-w-[375px] truncate whitespace-pre-wrap text-xl font-light">
+    <div class="flex w-[300px] flex-col gap-4 font-light lg:flex-row">
+      <h2
+        class="wrap line-clamp-2 max-w-[350px] whitespace-pre-line break-words text-xl font-light"
+      >
         {event.title}
       </h2>
-      <EventBadges {event} />
     </div>
-    <div class="flex flex-row items-end justify-between gap-4 pt-3 md:pt-0 lg:items-center">
-      <EventLogos {event} height={5} />
-      <ArrowRight class="mr-2" />
+    <div class="flex flex-row">
+      <EventBadges {event} />
     </div>
   </a>
 </li>
