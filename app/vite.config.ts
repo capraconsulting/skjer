05174782d70466f815cca2579d84343e9da2fedc
hermannm <hermann.morkrid@gmail.com>
2025-11-06 15:37:42 +0100

    Add Vitest for unit tests

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,14 @@
+/// <reference types="vitest/config" />
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  test: {
+    exclude: [
+      "**/node_modules/**",
+      "**/.git/**",
+      "**/e2e/**", // Vitest fails when trying to run our Playwright tests
+    ],
+  },
 });
